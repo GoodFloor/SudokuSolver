2e8969142889b2a6bdd0fbac8769dc6a36b7b8a0
GoodFloor <lukaszmachnik2@gmail.com>
2024-11-26 17:59:48 +0100

    praca inżynierska

--- Solver/mainG.cpp
+++ Solver/mainG.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 1000; i++)
     {
         Generator* generator = new Generator();
         GameBoard* board = generator->generateSudoku();
         delete generator;
-        Solver* solver = new Solver(board, false);
-        int r = solver->solve();
-        cout << r << "\n";
-        // board->printGrid();
-        delete solver;
+        // Solver* solver = new Solver(board, false);
+        // int r = solver->solve();
+        // cout << r << "\n";
+        board->printGrid();
+        // delete solver;
         delete board;
     }
     return 0;
--- Solver/mainS.cpp
+++ Solver/mainS.cpp
@@ -5,19 +5,24 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    GameBoard* board = new GameBoard();
-    GameBoard* solvedBoard = nullptr;
-    board->loadGrid();
-    board->printGrid();
-    Solver* solver = new Solver(board, false);
-    solver->printPossibilities();
-    int r = solver->solve();
-    
-    solvedBoard = solver->getBoard();
-    cout << r << ": \n";
-    solvedBoard->printGrid();
+    for (int i = 0; i < 1000; i++)
+    {
+        GameBoard* board = new GameBoard();
+        GameBoard* solvedBoard = nullptr;
+        board->loadGrid();
+        // board->printGrid();
+        Solver* solver = new Solver(board, false);
+        // solver->printPossibilities();
+        int r = solver->solve();
+        
+        // solvedBoard = solver->getBoard();
+        // cout << i << " = " << r << endl;
+        // solvedBoard->printGrid();
 
-    delete board;
-    delete solver;
+        delete board;
+        delete solver;
+    }
+    
+    
     return 0;
 }
